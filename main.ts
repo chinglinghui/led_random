10a841fd0d34dd74f318b0d7b0b4b6047b93a60e
chinglinghui <91246953+chinglinghui@users.noreply.github.com>
2021-12-16 16:32:35 +0800

    修正完成

--- main.ts
+++ main.ts
@@ -1,44 +1,43 @@
 /**
  * 我打算用不同的寫法
  */
+// 我讓
 input.onButtonPressed(Button.A, function () {
     if (燈數 < 24) {
         燈數 += 1
-        if (狀態 == 0) {
-            basic.clearScreen()
-            basic.showString("" + (燈數))
-        }
+        basic.showString("" + (燈數))
+        basic.clearScreen()
     }
 })
-// 先添加1~24的數字進表單
 input.onButtonPressed(Button.AB, function () {
-    狀態 = 1
     basic.clearScreen()
     led_random = []
+    // 先添加0~24進表單
     for (let index = 0; index <= 24; index++) {
         led_random.push(index)
     }
-    // 
+    // 然後隨機取一項
+    // 這個"隨機取的項"，以   "目標項"  代稱
     for (let index2 = 0; index2 < 燈數; index2++) {
-        亮起(randint(0, led_random.length))
+        亮起(randint(0, led_random.length - 1))
     }
-    basic.pause(1000)
-    狀態 = 0
+    basic.pause(200)
 })
 function 亮起 (索引值: number) {
+    // 得到目標項的值
     index = led_random[索引值]
+    // 把目標項移除
     led_random.removeAt(索引值)
-    X = index / 5
-    Y = (index + 1) % 5
+    // 得到X Y的值
+    X = Math.floor(index / 5)
+    Y = index % 5
     led.plot(X, Y)
 }
 input.onButtonPressed(Button.B, function () {
     if (燈數 > 1) {
         燈數 += -1
-        if (狀態 == 0) {
-            basic.clearScreen()
-            basic.showString("" + (燈數))
-        }
+        basic.showString("" + (燈數))
+        basic.clearScreen()
     }
 })
 let Y = 0
@@ -47,7 +46,4 @@ let index = 0
 let led_random: number[] = []
 let 燈數 = 0
 let 狀態 = 0
-狀態 = 0
-basic.forever(function () {
-	
-})
+燈數 = 1
